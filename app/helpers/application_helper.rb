1bad5d37349fe8b0b31e7bcf1c541768fb1a06fd
meriy100 <ttattataa@gmail.com>
2016-02-23 20:47:51 +0900

    三角にしたい

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,26 @@ module ApplicationHelper
     ]
     pipeline.call(source)[:output]
   end
+
+  # 選択中のサイドメニューのクラスを返す
+  def sidebar_activete(class_name)
+    is_active = false
+    case class_name
+    when :new_post
+      is_active = true if controller_name == "posts" and action_name == "new"
+    when :main
+      is_active = true if controller_name == "main"
+    when :posts
+      is_active = true if controller_name == "posts" and action_name != "new"
+    when :template
+      is_active = true if controller_name == "templates"
+    when :tag
+      is_active = true if controller_name == "tags"
+    when :logout
+      is_active = true if controller_name == "login"
+    end
+    "#{class_name}#{' active' if is_active}"
+  end
+
+
 end
