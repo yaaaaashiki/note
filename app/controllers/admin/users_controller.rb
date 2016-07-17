e6198dc0abf5a22344019cf5355110cb04db32d1
meriy100 <ttattataa@gmail.com>
2016-07-17 14:25:47 +0900

    admin権限でcreate

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,10 +9,15 @@ class Admin::UsersController < AdminController
   end
 
   def create
-    @user = User.new user_params
-    if @user.save
-      redirect_to admin_users_path
+    if user_params[:password] == params[:user][:password_agein]
+      @user = User.new user_params
+      if @user.save
+        redirect_to admin_users_path
+      else
+        render :new
+      end
     else
+      @user = User.new user_params
       render :new
     end
   end
