0915a9d7d0ec716f782d34eb9b75e57e32ac2d08
yaaaaashiki <yaaaaakishi@gmail.com>
2016-08-01 00:23:23 +0900

    adjust home view's post list output

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
     @root_tree = TreePost.all
-    @posts = Post.page(params[:page]).order(id: :desc)
+    @posts = Post.page(params[:page]).per(5).order(id: :desc)
   end
 end
